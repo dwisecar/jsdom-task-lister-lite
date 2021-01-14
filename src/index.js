b9ff980eacff6b5902dbe382dd483b38e01b6395
dwisecar <dwisecar@gmail.com>
2021-01-14 11:15:14 -0800

    stretch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById('new-task-description');
+  const toDoList = document.getElementById("tasks");
+  const prioritySelectInput = document.createElement("select"); 
+
+  addPrioritySelect();
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const taskLi = document.createElement("li");
+    taskLi.textContent = taskInput.value;
+    taskLi.style.color = prioritySelectInput.value;
+    toDoList.appendChild(taskLi);
+    taskForm.reset();
+  })
+
+  function addPrioritySelect() {
+    prioritySelectInput.add(new Option("High", "red"));
+    prioritySelectInput.add(new Option("Low", "green"));
+    prioritySelectInput.add(new Option("Medium", "yellow"));
+    taskForm.prepend(prioritySelectInput);
+  }
+
+
 });
+
+
